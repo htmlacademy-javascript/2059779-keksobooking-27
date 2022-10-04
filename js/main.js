1dc98a9dfc9828d363af3dd2f168d80e7a6ca872
Vladimir Trofimov <i_am@palant.org>
2022-10-04 10:22:23 +0300

    Adds second function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-const getDummyCoordinates = (min, max, points) => {
-  if (points < 0) {
-    points = points * -1;
+const generateRandomInt = (min, max) => {
+  if (min < 0 || max < 0 ) {
+    return NaN;
   }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.round((Math.random() * (max - min + 1) + min));
+};
+
+const generateDummyCoordinates = (min, max, points) => {
   if (points > 20) {
     points = 20;
   }
-  if (min < 0 || max < 0) {
+  if (min < 0 || max < 0 || points < 0) {
     return NaN;
   }
   if (min > max) {
@@ -14,4 +21,5 @@ const getDummyCoordinates = (min, max, points) => {
   return +((Math.random() * (max - min + 1) + min)).toFixed(points);
 };
 
-getDummyCoordinates(3, 4.5, 4);
+generateDummyCoordinates(3, 4.5, 4);
+generateRandomInt(2.5, 10);
