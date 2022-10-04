631a0f12b02e1e66600201fdaddccc731964df9c
Vladimir Trofimov <i_am@palant.org>
2022-10-04 12:03:22 +0300

    Changes swap and function name

--- js/main.js
+++ js/main.js
@@ -2,24 +2,30 @@ const generateRandomInt = (min, max) => {
   if (min < 0 || max < 0 || min === max) {
     return NaN;
   }
+  let swap;
   if (min > max) {
-    [min, max] = [max, min];
+    swap = max;
+    max = min;
+    min = swap;
   }
   return Math.round((Math.random() * (max - min + 1) + min));
 };
 
-const generateDummyCoordinates = (min, max, points) => {
+const generateRandomFloat = (min, max, points) => {
   if (points > 20) {
     points = 20;
   }
   if (min < 0 || max < 0 || points < 0 || min === max) {
     return NaN;
   }
+  let swap;
   if (min > max) {
-    [min, max] = [max, min];
+    swap = max;
+    max = min;
+    min = swap;
   }
   return +((Math.random() * (max - min + 1) + min)).toFixed(points);
 };
 
-generateDummyCoordinates(3, 4.5, 4);
+generateRandomFloat(3, 4.5, 4);
 generateRandomInt(2.5, 10);
