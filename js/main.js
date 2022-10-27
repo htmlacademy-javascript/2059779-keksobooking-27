cf26e09ca53b9439f3209eb88e1a3f465df5f49f
Vladimir Trofimov <i_am@palant.org>
2022-10-27 21:18:39 +0300

    Relocates offer generation function to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-import './popup.js';
+import { generateRandomOffers } from './mockup.js';
+import { markupOffers } from './popup.js';
+
+const similarOffers = generateRandomOffers(1);
+markupOffers(similarOffers);
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-import { generateRandomOffers } from './mockup.js';
 import { nounsDeclension } from './nouns-declension.js';
 
 const HOUSING_TYPES_TITLES = {
@@ -14,51 +13,53 @@ const offerTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-const similarOffers = generateRandomOffers(1);
+const markupOffers = (offersData) => {
 
-similarOffers.forEach(({ author, offer }) => {
-  const offerELement = offerTemplate.cloneNode(true);
-  offerELement.querySelector('.popup__title').textContent = offer.title; /** Посмотрел в ТЗ обязательные поля. Их нет смысла проверять. Или как минимум удалять. */
-  offerELement.querySelector('.popup__text--address').textContent = offer.address;
-  offerELement.querySelector('[data-price]').textContent = offer.price; /** В ретро рассказали о недостатках innerHTML и показали такой способ. */
-  offerELement.querySelector('.popup__type').textContent = HOUSING_TYPES_TITLES[offer.type];
-  offerELement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${nounsDeclension(offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${offer.guests} ${nounsDeclension(offer.guests, ['гостя', 'гостей', 'гостей'])}`;
-  offerELement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  offersData.forEach(({ author, offer }) => {
+    const offerELement = offerTemplate.cloneNode(true);
+    offerELement.querySelector('.popup__title').textContent = offer.title; /** Посмотрел в ТЗ обязательные поля. Их нет смысла проверять. Или как минимум удалять. */
+    offerELement.querySelector('.popup__text--address').textContent = offer.address;
+    offerELement.querySelector('[data-price]').textContent = offer.price; /** В ретро рассказали о недостатках innerHTML и показали такой способ. */
+    offerELement.querySelector('.popup__type').textContent = HOUSING_TYPES_TITLES[offer.type];
+    offerELement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${nounsDeclension(offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${offer.guests} ${nounsDeclension(offer.guests, ['гостя', 'гостей', 'гостей'])}`;
+    offerELement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
 
-  const featuresList = offerELement.querySelector('.popup__features');
-  const featuresListItem = featuresList.querySelector('.popup__feature');
-  featuresList.innerHTML = '';
-  featuresListItem.classList.remove(featuresListItem[1]);
-  if (offer.features && offer.features.length) {
-    const baseClass = featuresListItem.classList[0];
-    const modificatorClass = `${baseClass}--`;
-    offer.features.forEach((item) => {
-      featuresListItem.classList.add(`${modificatorClass}${item}`);
-      featuresList.append(featuresListItem.cloneNode(false));
-    });
-  }
+    const featuresList = offerELement.querySelector('.popup__features');
+    const featuresListItem = featuresList.querySelector('.popup__feature');
+    featuresList.innerHTML = '';
+    featuresListItem.classList.remove(featuresListItem[1]);
+    if (offer.features && offer.features.length) {
+      const baseClass = featuresListItem.classList[0];
+      const modificatorClass = `${baseClass}--`;
+      offer.features.forEach((item) => {
+        featuresListItem.classList.add(`${modificatorClass}${item}`);
+        featuresList.append(featuresListItem.cloneNode(false));
+      });
+    }
 
-  if (offer.description && offer.description.length) {
-    offerELement.querySelector('.popup__description').textContent = offer.description;
-  } else {
-    offerELement.querySelector('.popup__description').remove();
-  }
+    if (offer.description && offer.description.length) {
+      offerELement.querySelector('.popup__description').textContent = offer.description;
+    } else {
+      offerELement.querySelector('.popup__description').remove();
+    }
 
-  const photosContainer = offerELement.querySelector('.popup__photos');
-  const photoElement = photosContainer.querySelector('.popup__photo');
-  photosContainer.innerHTML = '';
-  if (offer.photos && offer.photos.length) {
-    offer.photos.forEach((photo) => {
-      photoElement.src = photo; /** Я видел, как другие студенты добавляют в alt текст заголовок объявления. Я с этим не согласен. Считаю, что лучше сюда подойдёт шаблонная фраза "Фотография жилья". */
-      photosContainer.append(photoElement.cloneNode(false));
-    });
-  }
+    const photosContainer = offerELement.querySelector('.popup__photos');
+    const photoElement = photosContainer.querySelector('.popup__photo');
+    photosContainer.innerHTML = '';
+    if (offer.photos && offer.photos.length) {
+      offer.photos.forEach((photo) => {
+        photoElement.src = photo; /** Я видел, как другие студенты добавляют в alt текст заголовок объявления. Я с этим не согласен. Считаю, что лучше сюда подойдёт шаблонная фраза "Фотография жилья". */
+        photosContainer.append(photoElement.cloneNode(false));
+      });
+    }
 
-  if (author.avatar && author.avatar.length) {
-    offerELement.querySelector('.popup__avatar').src = author.avatar;
-  } else {
-    offerELement.querySelector('.popup__avatar').remove();
-  }
+    if (author.avatar && author.avatar.length) {
+      offerELement.querySelector('.popup__avatar').src = author.avatar;
+    } else {
+      offerELement.querySelector('.popup__avatar').remove();
+    }
 
-  mapCanvas.append(offerELement);
-});
+    mapCanvas.append(offerELement);
+  });
+};
+export { markupOffers };
