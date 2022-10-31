c7df8f359f69842b08f6b690469cfb231e0e62f3
Vladimir Trofimov <i_am@palant.org>
2022-10-31 16:29:33 +0300

    Remakes card markup to first element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { generateRandomOffers } from './mockup.js';
 import { markupOffers } from './popup.js';
 
-const similarOffers = generateRandomOffers(1);
+const similarOffers = generateRandomOffers(3);
 markupOffers(similarOffers);
