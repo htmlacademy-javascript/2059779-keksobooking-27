3dbe8a958fd50f1f22a763bd68618d42cd4a5487
Vladimir Trofimov <i_am@palant.org>
2022-10-31 16:56:11 +0300

    Adds form switcher

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import { generateRandomOffers } from './mockup.js';
 import { markupOffers } from './popup.js';
+import { turnFormOff, turnFormOn } from './form-switcher.js';
 
 const similarOffers = generateRandomOffers(3);
 markupOffers(similarOffers);
+
+turnFormOff('.ad_form');
