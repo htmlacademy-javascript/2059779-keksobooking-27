4f1846d081f6e89602cb7338442df95b48900cac
Keks <keks@htmlacademy.ru>
2022-11-01 14:10:21 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/mockup.js
@@ -0,0 +1,139 @@
+import { generateRandomPositiveInt, generateRandomPositiveFloat, generateRandomArrayIndex } from './util.js';
+
+const TITLE_LEADINGS = [
+  'Отличный',
+  'Большой',
+  'Грязный',
+  'Бюджетный',
+  'Стильный',
+  'Викторианский',
+  'Готический',
+  'Кирпичный',
+  'Светлый',
+  'Проклятый старый'
+];
+
+const TITLE_ITEMS = [
+  'замок',
+  'дворец',
+  'пентхаус',
+  'хостел для харакири',
+  'уголок',
+  'притон',
+  'дом'
+];
+
+const TITLE_ENDINGS = [
+  'в центре Токио.',
+  'рядом с парком.',
+  'вашей мечты.',
+  'по небольшой цене.',
+  'недалеко от лепрозория.',
+  'только для лиц азиатской национальности.',
+  'в заросшем парке.'
+];
+
+const PRICE_MAX = 10000000;
+
+const HOUSING_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const ROOMS_MAX = 12;
+
+const GUESTS_MAX = 3;
+
+const CHECKIN_HOURS = {
+  early: '12:00',
+  standard: '13:00',
+  late: '14:00'
+};
+
+const CHECKOUT_HOURS = {
+  early: '12:00',
+  standard: '13:00',
+  late: '14:00'
+};
+
+const HOUSING_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const HOUSING_DESCRIPTIONS = [
+  'Уютное просторное и светлое помещение с современной бытовой техникой и стильными аксессуарами. Подойдёт для двух состоятельных людей, которые любят роскошь и ненавидят детей.',
+  'Евроремонт, евродвушка, евроокна, евродвери. К оплате только евро.',
+  'Дорогая мебель чехословакского производства, в отделке использованы лучшие экскрементальные материалы, отличные тихие соседи живут в соседнем районе.',
+  'Лучшего места для ритуального самоубийства не найти! Комплиментами гостям являются подарочный набор ножей для харакири, книга «Придумываем хайку за 30 секунд», удобное белое кимоно и корзинка для головы. Группам скидки. ПРЕДОПЛАТА!',
+  'Уникальное предложение на рынке! Дом с самым настоящим приведением! Нашего Каспера зовут дядя Стёпа. Жертва трагической любви к алкоголесодержащим напиткам. Ждите проказ от нашего домового в виде шатания мебели, сдачи в ломбард техники и невыключенной газовой плиты.',
+  'Любите шумную музыку? Вечеринки на всю ночь? Ваше хобби изготовление скульптур из железобетона при помощи перфоратора? У вашей жены от вашего хобби случается бурный оргазм? У вас есть коллекция шаров для боулинга, которой вы любите хвастать перед друзьями? Тогда эта квартира для вас! '
+];
+
+const HOUSING_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+const LOCATION_LATITUDE_RANGE = {
+  start: 35.65000,
+  end: 35.70000
+};
+
+const LOCATION_LONGITUDE_RANGE = {
+  start: 139.70000,
+  end: 139.80000
+};
+
+const LOCATION_PRECISION = 5;
+
+const ADS_COUNT = 10;
+
+const generateAvatarPath = () => `img/avatars/user${generateRandomPositiveInt(1, ADS_COUNT).toString().padStart(2, 0)}.png`;
+
+const generateRandomAuthor = () => ({
+  avatar: generateAvatarPath()
+});
+
+const generateRandomCoordinates = () => ({
+  lat: generateRandomPositiveFloat(LOCATION_LATITUDE_RANGE.start, LOCATION_LATITUDE_RANGE.end, LOCATION_PRECISION),
+  lng: generateRandomPositiveFloat(LOCATION_LONGITUDE_RANGE.start, LOCATION_LONGITUDE_RANGE.end, LOCATION_PRECISION)
+});
+
+const generateTitle = () => `${TITLE_LEADINGS[generateRandomPositiveInt(0, TITLE_LEADINGS.length - 1)]} ${TITLE_ITEMS[generateRandomArrayIndex(TITLE_ITEMS)]} ${TITLE_ENDINGS[Math.floor(Math.random() * TITLE_ENDINGS.length)]}`;
+
+const generateRandomHousing = (location) => ({
+  title: generateTitle(),
+  address: `${location.lat}, ${location.lng}`,
+  price: generateRandomPositiveInt(1, PRICE_MAX),
+  type: HOUSING_TYPES[generateRandomArrayIndex(HOUSING_TYPES)],
+  rooms: generateRandomPositiveInt(1, ROOMS_MAX),
+  guests: generateRandomPositiveInt(1, GUESTS_MAX),
+  checkin: Object.values(CHECKIN_HOURS)[generateRandomArrayIndex(Object.values(CHECKIN_HOURS))],
+  checkout: Object.values(CHECKOUT_HOURS)[generateRandomArrayIndex(Object.values(CHECKOUT_HOURS))],
+  features: HOUSING_FEATURES.slice(0, generateRandomPositiveInt(1, HOUSING_FEATURES.length)),
+  description: HOUSING_DESCRIPTIONS[generateRandomArrayIndex(HOUSING_DESCRIPTIONS)],
+  photos: HOUSING_PHOTOS.slice(0, generateRandomPositiveInt(1, HOUSING_PHOTOS.length)),
+});
+
+const generateRandomOffer = () => {
+  const coordinates = generateRandomCoordinates();
+
+  return {
+    author: generateRandomAuthor(),
+    offer: generateRandomHousing(coordinates),
+    location: coordinates
+  };
+};
+
+const generateRandomOffers = (count) => Array.from({ length: count }, generateRandomOffer);
+
+export { generateRandomOffers };
---
+++ 6/js/popup.js
@@ -0,0 +1,65 @@
+import { nounsDeclension } from './nouns-declension.js';
+
+const HOUSING_TYPES_TITLES = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
+
+const mapCanvas = document.querySelector('#map-canvas'); /** Наверное потом этой переменной нужно будет задать имя popup и другой селектор */
+const offerTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
+const markupOffers = (offersData) => {
+  const { author, offer } = offersData[0];
+
+  const offerELement = offerTemplate.cloneNode(true);
+  offerELement.querySelector('.popup__title').textContent = offer.title; /** Посмотрел в ТЗ обязательные поля. Их нет смысла проверять. Или как минимум удалять. */
+  offerELement.querySelector('.popup__text--address').textContent = offer.address;
+  offerELement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  offerELement.querySelector('.popup__type').textContent = HOUSING_TYPES_TITLES[offer.type];
+  offerELement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${nounsDeclension(offer.rooms, ['комната', 'комнаты', 'комнат'])} для ${offer.guests} ${nounsDeclension(offer.guests, ['гостя', 'гостей', 'гостей'])}`;
+  offerELement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
+  const featuresList = offerELement.querySelector('.popup__features');
+  const featuresListItem = featuresList.querySelector('.popup__feature');
+  featuresList.innerHTML = '';
+  featuresListItem.classList.remove(featuresListItem[1]);
+  if (offer.features && offer.features.length) {
+    const baseClass = featuresListItem.classList[0];
+    const modificatorClass = `${baseClass}--`;
+    offer.features.forEach((item) => {
+      featuresListItem.classList.add(`${modificatorClass}${item}`);
+      featuresList.append(featuresListItem.cloneNode(false));
+    });
+  }
+
+  if (offer.description && offer.description.length) {
+    offerELement.querySelector('.popup__description').textContent = offer.description;
+  } else {
+    offerELement.querySelector('.popup__description').remove();
+  }
+
+  const photosContainer = offerELement.querySelector('.popup__photos');
+  const photoElement = photosContainer.querySelector('.popup__photo');
+  photosContainer.innerHTML = '';
+  if (offer.photos && offer.photos.length) {
+    offer.photos.forEach((photo) => {
+      photoElement.src = photo; /** Я видел, как другие студенты добавляют в alt текст заголовок объявления. Я с этим не согласен. Считаю, что лучше сюда подойдёт шаблонная фраза "Фотография жилья". */
+      photosContainer.append(photoElement.cloneNode(false));
+    });
+  }
+
+  if (author.avatar && author.avatar.length) {
+    offerELement.querySelector('.popup__avatar').src = author.avatar;
+  } else {
+    offerELement.querySelector('.popup__avatar').remove();
+  }
+
+  mapCanvas.append(offerELement);
+
+};
+export { markupOffers };
