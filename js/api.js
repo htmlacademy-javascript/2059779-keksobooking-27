d2b60a2db266d043e4b84efbaf275a1ce8ee1e77
Vladimir Trofimov <i_am@palant.org>
2022-11-14 20:39:06 +0300

    Adds backend connection

--- js/api.js
+++ js/api.js
@@ -1,5 +1,19 @@
+const FAIL_TEXT = 'Не удалось отправить данные. Попробуйте ещё раз через некоторое время.';
+
 const getData = async () => {
   await fetch('https://27.javascript.pages.academy/keksobooking/data');
 };
 
-export { getData };
+const sendData = async (onFail, body) => {
+  try {
+    await fetch('https://27.javascript.pages.academy/keksobooking',
+      {
+        method: 'POST',
+        body,
+      });
+  } catch (error) {
+    onFail(FAIL_TEXT);
+  }
+};
+
+export { getData, sendData };
