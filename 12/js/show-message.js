d69f193e9866a93ff7cf05c8ad69a1a12c3a1d85
Keks <keks@htmlacademy.ru>
2022-11-20 09:22:57 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/show-message.js
+++ 12/js/show-message.js
@@ -42,6 +42,7 @@ const showErrorMessage = () => {
 
 const showAlertMessage = (message) => {
   const alertElement = document.createElement('div');
+  alertElement.setAttribute('role', 'alert');
   alertElement.classList.add('alert');
   alertElement.textContent = message;
   document.body.append(alertElement);
